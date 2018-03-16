d6d5a2d0d39132c07ce7c193b37dacf331c0c38d
jessica-liyan <13125157002@163.com>
2018-03-16 18:38:29 +0800

    添加radio组件

--- examples/docs/radio.md
+++ examples/docs/radio.md
@@ -1,6 +1,6 @@
 ## radio 单选按钮
 ### 基本用法
 <group>
-  <radio></radio>
+  <radio :options="['支付宝', '微信', 'QQ支付']"></radio>
 </group>
 
--- packages/radio/radio.vue
+++ packages/radio/radio.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>radio</div>
+  <div>
+    <cell class="radio"
+      v-for="(item, index) in options"
+      :for="`radio-${index}`"
+      :key="index"
+      :title="item"
+      @click.native="handleClick"
+    >
+      <input type="radio" :id="`radio-${index}`" ref="input">
+      <i class="iconfont icon-check"></i>
+    </cell>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Radio',
   props: {
+    options: Array
   },
   computed: {
   },
+  
   methods: {
+    handleClick () {
+      console.log(this.$refs.input)
+    }
   }
 }
 </script>
